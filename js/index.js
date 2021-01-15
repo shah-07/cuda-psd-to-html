0362a47842f94f0f1804e6d60c34b7ea8a4e9586
shahbaj00 <69644198+shahbaj00@users.noreply.github.com>
2021-01-15 20:30:27 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.querySelector(".header-area .header-logo .fas").addEventListener("click", function() {
+    document.querySelector(".header-area .logo-menu .header-menu").classList.toggle("show");
+})
