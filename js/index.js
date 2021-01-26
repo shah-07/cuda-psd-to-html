0ebfe646945a3fda4f5e305cb2c5fe79130fb2de
shahbaj00 <69644198+shahbaj00@users.noreply.github.com>
2021-01-26 12:43:01 +0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 document.querySelector(".header-area .header-logo .fas").addEventListener("click", function() {
     document.querySelector(".header-area .logo-menu .header-menu").classList.toggle("show");
     document.querySelector(".header-area .logo-menu .header-logo i").classList.toggle("fa-times");
-})
+});
+
+$(".portfolio-menu ul li a").click(function(){
+    return false;
+});
+
+var mixer = mixitup('.portfolios');
+var mixer = mixitup(containerEl);
+var mixer = mixitup(containerEl, {
+    selectors: {
+        target: '.blog-item'
+    },
+    animation: {
+        duration: 300
+    }
+});
